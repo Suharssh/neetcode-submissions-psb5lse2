9e48ad22912fa1d8fa7246d73d31b4658b8787fa
S Venkata Suharssh <121933150+Suharssh@users.noreply.github.com>
2026-06-01 22:24:21 +0530

    Add: lowest-common-ancestor-of-a-binary-tree-iii - submission-1

---
+++ Data Structures & Algorithms/lowest-common-ancestor-of-a-binary-tree-iii/submission-1.java
@@ -0,0 +1,26 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node parent;
+};
+*/
+
+class Solution {
+    public Node lowestCommonAncestor(Node p, Node q) {
+        Node pcopy = p;
+        Node qcopy = q;
+        while(pcopy != qcopy){
+            // if(qcopy == null) qcopy = p;
+            // else qcopy = qcopy.parent;
+            // if(pcopy == null) pcopy = q;
+            // else pcopy = pcopy.parent;
+
+            pcopy = (pcopy == null) ? q : pcopy.parent;
+            qcopy = (qcopy == null) ? p : qcopy.parent;
+        }
+        return pcopy;
+    }
+}
